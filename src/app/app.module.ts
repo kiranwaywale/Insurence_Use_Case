7f9c4dbd7b59e333d13be02be95bb12e87541c6b
charanhu <charanhumail@gmail.com>
2022-02-24 16:50:03 +0530

    Insurence_Case

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImageCarouselComponent } from './image-carousel/image-carousel.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { DashBordComponent } from './dash-bord/dash-bord.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent,
+    ImageCarouselComponent,
+    SideBarComponent,
+    DashBordComponent
   ],
   imports: [
     BrowserModule,
